dd4ddfb9d8493e2f8b9415c453e1cb69a898c7df
NTphat2004 <phongnguyen.0091@gmail.com>
2024-07-09 15:53:36 +0700

    cc

--- Solana-Group-7/src/main/java/com/example/demo/controller/LoginController.java
+++ Solana-Group-7/src/main/java/com/example/demo/controller/LoginController.java
@@ -93,7 +93,7 @@ public class LoginController {
 			}
 		}
 		else
-		{
+		{	
 			cookie.delete("username", rep);
 			cookie.delete("password", rep);
 		}
--- Solana-Group-7/src/main/java/com/example/demo/controller/SignUpController.java
+++ Solana-Group-7/src/main/java/com/example/demo/controller/SignUpController.java
@@ -1,16 +1,35 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.Entity.Users;
+import com.example.demo.Service.Cookies;
+import com.example.demo.repository.UserRebository;
 
 @Controller
 public class SignUpController {
-
-	@RequestMapping("/signUp")
-		public String SignInPage() {
+	
+	@GetMapping("/signUp")
+	public String SignUpIndex(@ModelAttribute("user") Users u) {	
 		
 		return"SignUp";
 	}
+	
+	@PostMapping("/doSign")
+	public  String doSign () {
+		
+		return"mailRegister";
+	}
+
 		
 	
 }
