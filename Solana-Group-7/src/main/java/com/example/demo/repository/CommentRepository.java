c6b0e0eca031df4aee531ac47170adc95ac0e5eb
Nguyen Van Loi <phuloc13620@gmail.com>
2024-07-16 07:10:41 +0700

    commit

--- Solana-Group-7/src/main/java/com/example/demo/repository/CommentRepository.java
+++ Solana-Group-7/src/main/java/com/example/demo/repository/CommentRepository.java
@@ -1,10 +1,18 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.Entity.Comments;
 import com.example.demo.Entity.Users;
 
 public interface CommentRepository extends JpaRepository<Comments, Integer> {
-
+	
+	@Query("SELECT cm FROM Comments cm where cm.nft.nftId  = ?1")
+	List<Comments> findCommentsByNFTs(Integer nftid);
+	
+	@Query("SELECT COUNT(n) FROM Comments n WHERE n.nft.nftId = :id")
+    Integer countByIdNft(Integer id);
 }
--- Solana-Group-7/src/main/java/com/example/demo/repository/NftRepository.java
+++ Solana-Group-7/src/main/java/com/example/demo/repository/NftRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.Entity.NFTs;
 import com.example.demo.Entity.Users;
 
 public interface NftRepository extends JpaRepository<NFTs, Integer> {
-
+	   @Query(value = "SELECT TOP 2 * FROM NFTs ORDER BY Price DESC", nativeQuery = true)
+	    List<NFTs> findTop2ByPrice();
+	
 }
